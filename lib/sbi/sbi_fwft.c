e08eedfec2ecbf962519036670d931fecb3ad9e0
Clément Léger <cleger@rivosinc.com>
2023-05-17 11:25:41 +0200

    lib: sbi: add support for firmware features extension
    This extension allows the software running in supervisor mode to control
    the behavior of various features of the SBI. First feature to be support
    is SBI_FW_FEATURE_MISALIGNED_DELEG firmware feature which allows to
    request delegation of misaligned traps handling to supervisor mode
    software. Delegating misaligned traps handling to the OS allows for
    finer grain control. For instance when using Linux, calling
    prctl(PR_SET_UNALIGN, PR_UNALIGN_SIGBUS) from userspace should change
    the behavior of misaligned accesses for the current process in order to
    generate a SIGBUS. Thanks to this extension, the misaligned handling can
    be delegated to Linux and allow it to handle that behavior properly.
    
    Signed-off-by: Clément Léger <cleger@rivosinc.com>

---
+++ lib/sbi/sbi_fwft.c
@@ -0,0 +1,152 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2024 Rivos Inc.
+ *
+ * Authors:
+ *   Clément Léger <cleger@rivosinc.com>
+ */
+
+#include <sbi/sbi_console.h>
+#include <sbi/sbi_bitmap.h>
+#include <sbi/sbi_ecall_interface.h>
+#include <sbi/sbi_error.h>
+#include <sbi/sbi_hart.h>
+#include <sbi/sbi_heap.h>
+#include <sbi/sbi_scratch.h>
+#include <sbi/sbi_string.h>
+#include <sbi/sbi_types.h>
+
+#include <sbi/riscv_asm.h>
+#include <sbi/riscv_encoding.h>
+
+/** Offset of pointer to FWFT HART state in scratch space */
+static unsigned long fwft_ptr_offset;
+
+#define fwft_get_hart_state_ptr(__scratch)				\
+	sbi_scratch_read_type((__scratch), void *, fwft_ptr_offset)
+
+#define fwft_thishart_state_ptr()					\
+	fwft_get_hart_state_ptr(sbi_scratch_thishart_ptr())
+
+#define fwft_set_hart_state_ptr(__scratch, __phs)			\
+	sbi_scratch_write_type((__scratch), void *, fwft_ptr_offset, (__phs))
+
+#define MIS_DELEG (1UL << CAUSE_MISALIGNED_LOAD | 1UL << CAUSE_MISALIGNED_STORE)
+
+#define SUPPORTED_FEATURE_COUNT	0
+
+struct fwft_config;
+
+struct fwft_feature {
+	enum sbi_fwft_feature_t id;
+	int (*supported)(struct fwft_config *conf);
+	int (*set)(struct fwft_config *conf, unsigned long value);
+	int (*get)(struct fwft_config *conf, unsigned long *value);
+};
+
+struct fwft_config {
+	const struct fwft_feature *feature;
+	unsigned long flags;
+};
+
+struct fwft_hart_state {
+	struct fwft_config configs[SUPPORTED_FEATURE_COUNT];
+};
+
+static struct fwft_config* get_feature_config(enum sbi_fwft_feature_t feature)
+{
+	int i;
+	struct fwft_hart_state *fhs = fwft_thishart_state_ptr();
+
+	if (feature & SBI_FWFT_GLOBAL_FEATURE_BIT)
+		return NULL;
+
+	for (i = 0; i < array_size(fhs->configs); i++){
+		if (feature == fhs->configs[i].feature->id)
+			return &fhs->configs[i];
+	}
+
+	return NULL;
+}
+
+int sbi_fwft_set(enum sbi_fwft_feature_t feature, unsigned long value,
+		 unsigned long flags)
+{
+	int ret;
+	struct fwft_config* conf;
+
+	conf = get_feature_config(feature);
+	if (!conf)
+		return SBI_EDENIED;
+
+	if (conf->feature->supported) {
+		ret = conf->feature->supported(conf);
+		if (ret)
+			return ret;
+	}
+
+	if ((flags & ~SBI_FWFT_SET_FLAG_LOCK) != 0)
+		return SBI_ERR_INVALID_PARAM;
+
+	if (conf->flags & SBI_FWFT_SET_FLAG_LOCK)
+		return SBI_EDENIED;
+
+	ret = conf->feature->set(conf, value);
+	if (ret)
+		return ret;
+
+	conf->flags = flags;
+
+	return SBI_OK;
+}
+
+int sbi_fwft_get(enum sbi_fwft_feature_t feature, unsigned long *out_val)
+{
+	int ret;
+	struct fwft_config* conf;
+
+	conf = get_feature_config(feature);
+	if (!conf)
+		return SBI_EDENIED;
+
+	if (conf->feature->supported) {
+		ret = conf->feature->supported(conf);
+		if (ret)
+			return ret;
+	}
+
+	return conf->feature->get(conf, out_val);
+}
+
+static const struct fwft_feature features[] = {};
+
+_Static_assert(
+	array_size(features) == SUPPORTED_FEATURE_COUNT,
+	"features[] array must be the same size as SUPPORTED_FEATURE_COUNT"
+	);
+
+int sbi_fwft_init(struct sbi_scratch *scratch, bool cold_boot)
+{
+	int i;
+	struct fwft_hart_state *fhs;
+
+	if (cold_boot) {
+		fwft_ptr_offset = sbi_scratch_alloc_type_offset(void *);
+		if (!fwft_ptr_offset)
+			return SBI_ENOMEM;
+	}
+
+	fhs = fwft_get_hart_state_ptr(scratch);
+	if (!fhs) {
+		fhs = sbi_zalloc(sizeof(*fhs));
+		if (!fhs)
+			return SBI_ENOMEM;
+		for (i = 0; i < array_size(features); i++)
+			fhs->configs[i].feature = &features[i];
+
+		fwft_set_hart_state_ptr(scratch, fhs);
+	}
+
+	return 0;
+}
