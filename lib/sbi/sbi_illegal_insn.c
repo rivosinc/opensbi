091b3564ef0fce972bbcfdc571b5c98c1fbe4c58
Anup Patel <apatel@ventanamicro.com>
2024-03-12 15:58:00 +0530

    lib: sbi: Simplify parameters of sbi_illegal_insn_handler()
    The struct sbi_trap_context already has the information needed by
    sbi_illegal_insn_handler() so directly pass struct sbi_trap_context
    pointer to this function.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>

--- lib/sbi/sbi_illegal_insn.c
+++ lib/sbi/sbi_illegal_insn.c
@@ -136,8 +136,9 @@ static const illegal_insn_func illegal_insn_table[32] = {
 	truly_illegal_insn  /* 31 */
 };
 
-int sbi_illegal_insn_handler(ulong insn, struct sbi_trap_regs *regs)
+int sbi_illegal_insn_handler(struct sbi_trap_context *tcntx)
 {
+	ulong insn = tcntx->trap.tval;
 	struct sbi_trap_info uptrap;
 
 	/*
@@ -153,13 +154,12 @@ int sbi_illegal_insn_handler(ulong insn, struct sbi_trap_regs *regs)
 
 	sbi_pmu_ctr_incr_fw(SBI_PMU_FW_ILLEGAL_INSN);
 	if (unlikely((insn & 3) != 3)) {
-		insn = sbi_get_insn(regs->mepc, &uptrap);
-		if (uptrap.cause) {
-			return sbi_trap_redirect(regs, &uptrap);
-		}
+		insn = sbi_get_insn(tcntx->regs.mepc, &uptrap);
+		if (uptrap.cause)
+			return sbi_trap_redirect(&tcntx->regs, &uptrap);
 		if ((insn & 3) != 3)
-			return truly_illegal_insn(insn, regs);
+			return truly_illegal_insn(insn, &tcntx->regs);
 	}
 
-	return illegal_insn_table[(insn & 0x7c) >> 2](insn, regs);
+	return illegal_insn_table[(insn & 0x7c) >> 2](insn, &tcntx->regs);
 }
